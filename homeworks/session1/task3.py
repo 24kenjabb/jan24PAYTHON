183b939209ef706c59bc78617d7f6da1ad139fe2
24kenjabb <24kenjabb@gmail.com>
2024-02-28 22:47:27 +0000

    updating

--- homeworks/session1/task3.py
+++ homeworks/session1/task3.py
@@ -1,4 +1,6 @@
-str1="<<>>"
-str2="hello"
-result=str1[:2]+str2+str1[2:]
-print(result)
+test_string = str(input("Please enter a string for the variable chars: "))
+print("The original string is: " + test_string)
+first_part, second_part = test_string[:len(test_string)//2], test_string[len(test_string)//2:]
+Word = str(input("Please enter a word to insert into the middle of chars: ")) 
+
+print(first_part + Word + second_part)
