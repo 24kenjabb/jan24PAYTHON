c870b127cfe331f7431c0185b45075bb02e66da0
24kenjabb <24kenjabb@gmail.com>
2024-03-05 23:14:14 +0000

    updating

---
+++ homeworks/session4.py
@@ -0,0 +1,73 @@
+#tup = ("abbos",)
+#print(type(tup))
+
+#tup = ("ABBOS,")
+#Jan24_names = (
+ #   "Airperi",
+  #  "Husrav",
+   # "Fatima",
+  #  "Dastan",
+  #  "Guliza",
+   # "Merrim",
+   # "Tatina"
+#)
+
+#print(Jan24_names.count("Tatina",))
+
+#person = ("John", "Doe", "01-01-1990", "123-1234",)      ##tried to change a tuple to see whats gonna happen "you can only change list [] and not tuple ()"
+#person[0] = "Johnson"
+#print(person)
+
+
+#person = ("John", "Doe", "01-01-1990", "123-1234",)
+#print(type(person))
+#for info in person:     ## just ran a for loop
+   # print(info)
+
+#person = {
+   # "name": "abbos",
+   # "lastname": "Kenjaev",
+   # "DOB": "03-14-2006"
+#}
+#print(person)
+
+#person = {
+  #  "name": "abbos",
+    #"lastname": "Kenjaev",
+   # "DOB": "03-14-2006",
+   # "phone": ["213-123-4432", "341-234-1234", "141-314-1243"]
+#}
+#print(person)
+
+
+
+#movie = ["wonderland", "2000", "James bush"]
+#print(type(movie))
+
+
+#movies = {
+  #  "name": "formula1",
+ #   "actor": "Tom hanks",
+ #   "author": "James Bush"
+#}
+#print(movies)
+
+#person = {
+  #  "name": "abbos",
+ #   "lastname": "Kenjaev",
+ #   "DOB": "03-14-2006",
+  #  "phone": ["213-123-4432", "341-234-1234", "141-314-1243"]
+#}
+#for info in person.items():
+   # print(info)
+
+#info = {
+   # "Name": "Munira",
+  #  "lastname": "Kenjaeva",
+  #  "DOB": "02/27/2004",
+  #  "PlaceOfBirth": "Uzbekistan"
+#}
+#for infoo in info.items():  ##items for both keys and answer
+  #  print(infoo)
+
+
